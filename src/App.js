78894b649dedbee9709b55283b49eba29d690ab3
Web Development <aquisition@Heets-MacBook-Air.local>
2020-06-21 13:27:00 +0530

    Make out separate folders for pages and component, homepage updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import Homepage from  './Homepage.component';
+import Homepage from  './Pages/Homepage.component';
+import {BrowserRouter as Router,Route} from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <Homepage/>
+        <Router >
+           <Route exact path='/'  component={Homepage} / >
+        </Router>
+        
     </div>
   );
 }
---
+++ src/Pages/Homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './homepage.styles.scss'
+import Directory from '../Components/directary/directory.component'
+
+const Homepage =  () => (
+    <div className='homepage'>
+        <Directory/>
+    </div>
+)
+
+export default Homepage;
